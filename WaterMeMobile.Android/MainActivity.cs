9caec9dc720b93e6d3d774922e3bdd458c63c593
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2020-09-21 22:53:39 +0200

    resolve conflicts

--- WaterMeMobile.Android/MainActivity.cs
+++ WaterMeMobile.Android/MainActivity.cs
@@ -9,8 +9,12 @@ using Android.OS;
 
 namespace WaterMeMobile.Droid
 {
-    //Fork check
-    [Activity(Label = "WaterMeMobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    
+    [Activity(Label = "WaterMeMobile",
+     Icon = "@drawable/background",
+     Theme = "@style/MainTheme",
+     MainLauncher = false,
+     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
