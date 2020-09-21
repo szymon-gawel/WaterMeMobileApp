548b673873a5faa3cc40523a7bcf0f2ab31ea8b5
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2020-09-21 22:18:59 +0200

    MainLauncher = true

--- WaterMeMobile.Android/MainActivity.cs
+++ WaterMeMobile.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 namespace WaterMeMobile.Droid
 {
     [Activity(Label = "WaterMe", Icon = "@mipmap/icon", Theme = "@style/MainTheme", 
-        MainLauncher = false, 
+        MainLauncher = true, 
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
